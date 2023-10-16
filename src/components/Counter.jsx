6058eebf6665ef2caebe9d32acf5d17f541590e1
soniagkalliath <soniageorge111@gmail.com>
2023-10-16 10:10:16 +0530

    counter with redux toolkit design update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,10 +9,10 @@ function Counter() {
     <div style={{height:'70vh'}} className='w-100 d-flex justify-content-center align-items-center '>
        <div className='border rounded p-5 w-25 d-flex justify-content-center align-items-center flex-column'> 
             <h1>{count}</h1>
-            <div className="d-flex justify-content-between w-100 mt-5">
+            <div className="d-flex  w-100 mt-5">
                 <button onClick={()=>dispatch(decrement())} className='btn btn-warning'>Decrement</button>
-                <button onClick={()=>dispatch(reset())} className='btn btn-danger'>Reset</button>
-                <button onClick={()=>dispatch(increment())} className='btn btn-success'>Increment</button>
+                <button onClick={()=>dispatch(reset())} className='btn btn-danger ms-3'>Reset</button>
+                <button onClick={()=>dispatch(increment())} className='btn btn-success ms-3'>Increment</button>
             </div>
             <button onClick={()=>dispatch(incrementByAmount(5))} className='btn btn-primary mt-4'>Increment By Amount</button>
        </div>
